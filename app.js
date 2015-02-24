7b300a9802391e507b4e999e6edc59f158dc4cdd
Muthukumar Rajendran <kumar_muthu@hotmail.com>
2015-02-24 15:13:10 +0000

    updated index.html

--- app.js
+++ app.js
@@ -1,61 +1,80 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngRoute']);
 
-app.controller("myCtrl", function ($scope) {
-    $scope.games = [
-        {
-            "name": "Hockey",
-            "selected": false
-        },
-        {
-            "name": "Football",
-            "selected": false
-        },
-        {
-            "name": "Tennis",
-            "selected": false
-        }];
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/two-way-binding', {
+            templateUrl: 'partials/two-way-binding.html',
+            controller: 'myCtrl'
+        }).
+        when('/templates', {
+            templateUrl: 'partials/templates.html',
+            controller: 'PhoneDetmyCtrlailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/two-way-binding'
+        });
+    }
+]);
 
-    var _players = [
-          {
-              "name": "Jane Doe",
-              "Age": 25,
-              "game": "Hockey",
-              "selected": false
-          },
-          {
-              "name": "Sam John",
-              "Age": 25,
-              "game": "Football",
-              "selected": false
-          },
-          {
-              "name": "Peter Robin",
-              "Age": 27,
-              "game": "Football",
-              "selected": false
-          },
-          {
-              "name": "Jack Dan",
-              "Age": 27,
-              "game": "Tennis",
-              "selected": false
-          },
-          {
-              "name": "Sara Johnson",
-              "Age": 25,
-              "game": "Tennis",
-              "selected": false
-          },
-          {
-              "name": "Jessie Jack",
-              "Age": 25,
-              "game": "Football",
-              "selected": false
-          }
-    ];
-    
-    $scope.gameSelected = function () {
-        var selected_games = _.filter($scope.games, function (game) { return game.selected }).map(function (x) { return x.name });
-        $scope.players = _.filter(_players, function (player) { return _.contains(selected_games, player.game) });
+
+app.controller("myCtrl", function($scope) {
+    $scope.games = [{
+        "name": "Hockey",
+        "selected": false
+    }, {
+        "name": "Football",
+        "selected": false
+    }, {
+        "name": "Tennis",
+        "selected": false
+    }];
+
+    var _players = [{
+        "name": "Jane Doe",
+        "Age": 25,
+        "game": "Hockey",
+        "selected": false
+    }, {
+        "name": "Sam John",
+        "Age": 25,
+        "game": "Football",
+        "selected": false
+    }, {
+        "name": "Peter Robin",
+        "Age": 27,
+        "game": "Football",
+        "selected": false
+    }, {
+        "name": "Jack Dan",
+        "Age": 27,
+        "game": "Tennis",
+        "selected": false
+    }, {
+        "name": "Sara Johnson",
+        "Age": 25,
+        "game": "Tennis",
+        "selected": false
+    }, {
+        "name": "Jessie Jack",
+        "Age": 25,
+        "game": "Football",
+        "selected": false
+    }];
+
+    $scope.gameSelected = function() {
+        var selected_games = _.filter($scope.games, function(game) {
+            return game.selected
+        }).map(function(x) {
+            return x.name
+        });
+        $scope.players = _.filter(_players, function(player) {
+            return _.contains(selected_games, player.game)
+        });
     }
 });
+
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+})
